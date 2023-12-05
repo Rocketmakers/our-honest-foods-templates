25abe959a04f4e357ecad4c885838ac2e3933bd2
Rocketmaker <rocketmaker@MacBook-Pro-2.local>
2023-12-05 15:20:13 +0000

    changed the model data to [][]

--- layouts/saleOrder/model.ts
+++ layouts/saleOrder/model.ts
@@ -17,23 +17,43 @@ export interface IModel {
   productAssemblyNotes: string[];
 }
 
-export const sampleData: IModel[] = [
-  {
-    orderName: "ORD-123456",
-    accountName: "Example Customer",
-    siteName: "Site A",
-    address: "123 Main Street",
-    accessNotes: "Go round the back and knock on the door",
-    deliveryDate: "2023-11-10",
-    rows: [
-      { product: "Product A", quantity: "5" },
-      { product: "Product B", quantity: "2" },
-      { product: "Product C", quantity: "3" },
-    ],
-    productAssemblyNotes: [
-      "This is a note for product A",
-      "This is a note for product B",
-      "This is a note for product C",
-    ],
-  },
+export const sampleData: IModel[][] = [
+  [
+    {
+      orderName: "ORD-123456",
+      accountName: "Example Customer",
+      siteName: "Site A",
+      address: "123 Main Street",
+      accessNotes: "Go round the back and knock on the door",
+      deliveryDate: "2023-11-10",
+      rows: [
+        { product: "Product A", quantity: "5" },
+        { product: "Product B", quantity: "2" },
+        { product: "Product C", quantity: "3" },
+      ],
+      productAssemblyNotes: [
+        "This is a note for product A",
+        "This is a note for product B",
+        "This is a note for product C",
+      ],
+    },
+    {
+      orderName: "ORD-09876543",
+      accountName: "Example Customer Two",
+      siteName: "Site B",
+      address: "123 Main Street",
+      accessNotes: "Go round the front and knock on the door",
+      deliveryDate: "2023-11-10",
+      rows: [
+        { product: "Product A", quantity: "5" },
+        { product: "Product B", quantity: "2" },
+        { product: "Product C", quantity: "3" },
+      ],
+      productAssemblyNotes: [
+        "This is a note for product A",
+        "This is a note for product B",
+        "This is a note for product C",
+      ],
+    },
+  ],
 ];
